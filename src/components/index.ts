946fa7e9b997553d784e98c0146b589247bed7df
Elis Antonio Perez <elisperezmusic@gmail.com>
2024-02-01 22:03:00 -0500

    ProductGridItem

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export * from './ui/top-menu/TopMenu';
 export * from './ui/not-found/NotFound';
 export * from './ui/title/Title';
+export * from './products/product-grid/ProductGrid';
+export * from './products/product-grid/ProductGridItem';
